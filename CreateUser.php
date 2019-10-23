46cc2c6b1e3d1cb9fe5efcea8e732b9679d3cc0a
Meet Kapadia <mkapadia70@ku.edu>
2019-10-23 10:16:24 -0500

    bug fixes

--- CreateUser.php
+++ CreateUser.php
@@ -3,14 +3,18 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 $new_user_ID = $_POST['username'];
 $mysqli = new mysqli("mysql.eecs.ku.edu", "mkapadia70", "ooH3aeK3", "mkapadia70");
-if($mysqli->connect_errno) {
+if($mysqli->connect_errno)
+{
      printf("Connection failed: %s\n" , $mysqli->connect_error);
      exit();
 }
 $query = "SELECT user_id FROM Users WHERE user_id='$new_user_ID'";
-if($result = $mysqli->query($query)->num_rows > 0) {
+if($result = $mysqli->query($query)->num_rows > 0)
+{
      echo "{$_POST['username']} is already in use.";
-} else {
+}
+else
+{
      $addUser = "INSERT INTO Users (user_id) VALUES ('$new_user_ID')";
      $mysqli->query($addUser);
      echo "{$_POST['username']} is now added to the database";
