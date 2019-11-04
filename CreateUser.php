a15417f61ed60f83f7926ace885866542c765995
Meet Kapadia <mkapadia70@ku.edu>
2019-11-04 14:24:03 -0600

    more bug fixes

--- CreateUser.php
+++ CreateUser.php
@@ -1,23 +1,24 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-$new_user_ID = $_POST['username'];
-$mysqli = new mysqli("mysql.eecs.ku.edu", "mkapadia70", "ooH3aeK3", "mkapadia70");
-if($mysqli->connect_errno)
-{
-     printf("Connection failed: %s\n" , $mysqli->connect_error);
-     exit();
+$username = $_POST['username'];
+if(!empty($username)){
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "mkapadia70", "ooH3aeK3", "mkapadia70");
+    if($mysqli->connect_errno){
+        echo "Connection failed: " . $mysqli->connect_error;
+        exit();
+    }
+
+    $query = "INSERT INTO Users (user_id) VALUES ('" . $username . "');";
+    if($result = $mysqli->query($query)){
+        echo "User created successfully";
+    }
+    else{
+        echo "That username is already taken";
+    }
+    $mysqli->close();
 }
-$query = "SELECT user_id FROM Users WHERE user_id='$new_user_ID'";
-if($result = $mysqli->query($query)->num_rows > 0)
-{
-     echo "{$_POST['username']} is already in use.";
+else{
+    echo "Create error: username cannot be blank";
 }
-else
-{
-     $addUser = "INSERT INTO Users (user_id) VALUES ('$new_user_ID')";
-     $mysqli->query($addUser);
-     echo "{$_POST['username']} is now added to the database";
-}
-$mysqli->close();
 ?>
